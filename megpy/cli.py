2b480866b7cb2721630b9eb3277d6d3fee94cacf
Garud Snoep <gsnoep42@gmail.com>
2023-05-25 14:56:31 +0200

    cli: updated CLI to match API changes; file, method and x_fs are now positional instead of flagged arguments

--- megpy/cli.py
+++ megpy/cli.py
@@ -23,19 +23,19 @@ def str2bool(v):
 
 def parse():
     parser = argparse.ArgumentParser(description='MEGPy: Extract flux surface shaping parameters (Miller, Turnbull-Miller, etc) from EQDSK files.')
-    parser.add_argument('-m', '--method', dest='method', required=False, type=str, default='turnbull', help='parameterization to be used for fitting the flux surfaces and Bpol', metavar='STR')
-    parser.add_argument('-f', '--file_path', dest='file', required=True, type=is_valid_file, help='valid path to an EQDSK file', metavar='FILE')
+    parser.add_argument(dest='file', type=is_valid_file, help='valid path to an EQDSK file', metavar='FILE')
+    parser.add_argument(dest='method', type=str, default='turnbull', help='parameterization to be used for fitting the flux surfaces and Bpol', metavar='STR')
+    parser.add_argument(dest='x_fs', type=float, help='radial location to extract flux surface and Bpol shaping parameters', metavar='POSITION')
+    parser.add_argument('-x', '--x_label', dest='x_label', required=False, type=str, default='rho_tor', help='label of the radial location (e.g. rho_tor, rho_pol, r/a)', metavar='STR')
     parser.add_argument('-r', '--refine', dest='refine', required=False, type=int, default=0, help='refine the poloidal flux map psi(R,Z) <r> times', metavar='INT')
-    parser.add_argument('-x', '--x_fluxsurface', dest='x_fs', required=True, type=float, help='radial location to extract flux surface and Bpol shaping parameters', metavar='POSITION')
-    parser.add_argument('-l', '--x_label', dest='x_label', required=False, type=str, default='rho_tor', help='label of the radial location (e.g. rho_tor, rho_pol, r/a)', metavar='STR')
-    parser.add_argument('-p', '--printer', dest='printer', required=False, type=str, default='GENE', help='radial location to extract flux surface shaping parameters', metavar='STR')
-    parser.add_argument('--printer_lref', dest='lref', required=False, type=str, default='a', help='radial location to extract flux surface shaping parameters', metavar='STR')
-    parser.add_argument('-c', '--cost_f', dest='cost_f', required=False, type=str, default='l1l2', help='radial location to extract flux surface shaping parameters', metavar='STR')
-    parser.add_argument('--n_x', dest='n_x', required=False, type=int, default=10, help='number of radial points used for radial derivatives', metavar='INT')
-    parser.add_argument('--n_theta', dest='n_theta', required=False, type=int, default=7200, help='number of radial points used for radial derivatives', metavar='INT')
-    parser.add_argument('--n_harmonics', dest='n_harmonics', required=False, type=int, default=2, help='number of radial points used for radial derivatives', metavar='INT')
+    parser.add_argument('--n_x', dest='n_x', required=False, type=int, default=9, help='number of radial points used for radial derivatives', metavar='INT')
+    parser.add_argument('--n_theta', dest='n_theta', required=False, type=int, default=7200, help='number of poloidal grid points', metavar='INT')
+    parser.add_argument('--n_harmonics', dest='n_harmonics', required=False, type=int, default=3, help='number of harmonics used in the optimization for fourier/miller_general/mxh', metavar='INT')
+    parser.add_argument('-p', '--printer', dest='printer', required=False, type=str, default='GENE', help='name of the code to print the geometry input values for', metavar='STR')
+    parser.add_argument('--printer_lref', dest='lref', required=False, type=str, default='a', help='reference length Lref used to normalized code input values', metavar='STR')
     parser.add_argument('-a', '--analytic-geo', dest='analytic_geo', required=False, nargs='?', const=True, default=True, type=str2bool, help='include the analytical Miller parameters for comparison y/n', metavar='BOOL')
-    parser.add_argument('--opt-bpol', dest='opt_bpol', required=False, nargs='?', const=False, default=False, type=str2bool, help='refine the poloidal flux map psi(R,Z) <i> times', metavar='BOOL')
+    parser.add_argument('--opt-bpol', dest='opt_bpol', required=False, nargs='?', const=False, default=False, type=str2bool, help='optimize shape derivative parameters by minimizing the error in Bpol', metavar='BOOL')
+    parser.add_argument('--opt-deriv', dest='opt_deriv', required=False, nargs='?', const=False, default=False, type=str2bool, help='optimize shape derivative parameters by minimizing the error in dRdr, dZdr, dRdtheta, dZdtheta', metavar='BOOL')
     args = parser.parse_args()
 
     eq = Equilibrium()
@@ -49,24 +49,26 @@ def parse():
         eq.add_fluxsurfaces()
 
     locgeo = LocalEquilibrium(args.method,
-                           eq,
-                           args.x_fs,
-                           x_label=args.x_label,
-                           cost_f=args.cost_f,
-                           n_x=args.n_x,
-                           n_theta=args.n_theta,
-                           n_harmonics=args.n_harmonics,
-                           incl_analytic_geo=args.analytic_geo,
-                           opt_bpol=args.opt_bpol,
-                           verbose=0)
+                              eq,
+                              args.x_fs,
+                              x_label=args.x_label,
+                              n_x=args.n_x,
+                              n_theta=args.n_theta,
+                              n_harmonics=args.n_harmonics,
+                              analytic_shape=args.analytic_geo,
+                              opt_bpol=args.opt_bpol,
+                              opt_deriv=args.opt_deriv,
+                              diag_lsq=0,
+                              verbose=True)
+    
     
     if args.printer:
-        locgeo.printer(args.printer,locgeo.shape,locgeo.param_labels,locgeo.shape_bpol,locgeo.bpol_labels)
+        locgeo.printer(args.printer,locgeo.shape,locgeo.param_labels,locgeo.shape_deriv,locgeo.deriv_labels)
         #if incl_analytic_geo:
-        #    self.printer(printer,self.shape_analytic,self.param_labels,self.shape_bpol_analytic,self.bpol_labels)
+        #    self.printer(printer,self.shape_analytic,self.param_labels,self.shape_deriv_analytic,self.deriv_labels)
         
-    if not args.no_plots:
-        locgeo.plot_all(incl_analytic_geo=args.analytic_geo,opt_bpol=args.opt_bpol)
+    #if not args.no_plots:
+    #    locgeo.plot_all(incl_analytic_geo=args.analytic_geo,opt_bpol=args.opt_bpol)
 
 
 
