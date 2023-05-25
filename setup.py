21b2ec39dff8fff7d1a263377aae322025cefafb
Garud Snoep <gsnoep42@gmail.com>
2023-05-25 15:21:30 +0200

    setup: updated version information

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup,find_packages
 
 setup(
     name='megpy',
-    version='2022.6.1',
+    version='1.1.0',
     description='MEGPy',
-    url='https://www.github.com/FusionKit/fusionkit',
+    url='https://www.github.com/gsnoep/megpy',
     author='Garud Snoep',
     classifiers=['Programming Language :: Python :: 3', 
                 'Operating System :: OS Independent'],
     keywords='fusion simulation toolkit',
     packages=find_packages(),
     package_dir={'megpy':'megpy'},
-    install_requires = ['numpy', 'scipy', 'pandas'],
+    install_requires = ['numpy', 'scipy'],
 )
